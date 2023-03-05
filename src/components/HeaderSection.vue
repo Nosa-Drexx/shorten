33332112ce538091df1ce5e7ca0c0581700833f1
Nosa-Drexx <nosakhare.egharevba@physci.uniben.edu>
2023-03-05 20:27:33 +0100

    header and nav design for bigger screens

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -2,13 +2,14 @@
 import { defineComponent } from 'vue'
 import AnimatedButton from './AnimatedButton.vue'
 import NavSection from './NavSection.vue'
+import NavSectionBiggerScreens from './NavSectionBiggerScreens.vue'
 
 type DataState = {
   showNav: boolean
 }
 
 export default defineComponent({
-  components: { NavSection, AnimatedButton },
+  components: { NavSection, AnimatedButton, NavSectionBiggerScreens },
   data: (): DataState => ({
     showNav: false
   }),
@@ -26,6 +27,7 @@ export default defineComponent({
     <div class="animated-button-container">
       <AnimatedButton @button-status="showOrHideNav" />
     </div>
+    <NavSectionBiggerScreens class="nav-bigger-screen" />
   </header>
   <NavSection v-if="showNav" :status="showNav" />
 </template>
@@ -38,11 +40,17 @@ export default defineComponent({
   @include flex(row nowrap, space-between);
   color: $primaryColor;
   padding: 1rem;
+  .nav-bigger-screen {
+    display: none;
+  }
 }
 @media (min-width: 800px) {
   .header {
     .animated-button-container {
       display: none;
+    }
+    .nav-bigger-screen {
+      display: flex;
     }
   }
 }
---
+++ src/components/NavSectionBiggerScreens.vue
@@ -0,0 +1,64 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+type DataState = {
+  navList: readonly ['About', 'Contact']
+}
+
+export default defineComponent({
+  data: (): DataState => ({
+    navList: ['About', 'Contact']
+  })
+})
+</script>
+
+<template>
+  <nav class="animated-nav" ref="nav">
+    <ul class="ul">
+      <li class="li" v-for="list in navList" :key="list">
+        <span class="bottom-border"></span>
+        <a :href="`#${list}`">{{ list }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/colors';
+@import '@/assets/fonts';
+@import '@/assets/mixins';
+.animated-nav {
+  @include flex();
+  width: 40%;
+  ul {
+    @include flex(row nowrap, space-evenly);
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    li {
+      position: relative;
+      padding: 0;
+      margin: 0;
+      list-style-type: none;
+      .bottom-border {
+        display: flex;
+        position: absolute;
+        bottom: -3px;
+        width: 0px;
+        transition: width 0.5s;
+      }
+      a {
+        @include font(500);
+        text-decoration: none;
+        font-size: $size1;
+        color: black;
+      }
+    }
+    li:hover {
+      .bottom-border {
+        width: 30px;
+        border-bottom: 3px solid $primaryColor;
+      }
+    }
+  }
+}
+</style>
