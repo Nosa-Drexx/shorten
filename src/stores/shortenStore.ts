c24e286e80ec7188005e3337c66bf19cec2bcf82
Nosa-Drexx <nosakhare.egharevba@physci.uniben.edu>
2023-03-05 16:11:20 +0100

    initial Commit

---
+++ src/stores/shortenStore.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+
+export const useShortenStore = defineStore('shortenStore', {
+  //State
+  state: () => ({}),
+
+  //Computed
+  getters: {},
+
+  //Methods
+  actions: {}
+})
